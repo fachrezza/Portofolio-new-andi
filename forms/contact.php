6d1baaf238d1fe40f65c29460f698c701673e48c
fachrezza <100120994+fachrezza@users.noreply.github.com>
2025-04-09 18:06:30 +0700

    Update contact.php
    update to recieve an email

--- forms/contact.php
+++ forms/contact.php
@@ -1,27 +1,29 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name    = htmlspecialchars($_POST['name']);
+    $email   = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
 
+    // Email tujuan (ganti dengan email kamu)
+    $to = "fachrezandi123@gmail.com";
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'fachrezandi123@gmail.com';
+    // Header email
+    $headers = "From: $name <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+    // Isi email
+    $body = "Name: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Subject: $subject\n\n";
+    $body .= "Message:\n$message\n";
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+    // Kirim email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Your message has been sent. Thank you!";
+    } else {
+        echo "Failed to send message. Please try again.";
+    }
+}
 ?>
